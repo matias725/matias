210e4190e828945a46b4d866c76c7558690276fc
matias725 <alexisbordones18@gmail.com>
2025-11-05 23:31:35 +0100

    feat: Complete CRUD system with 4-layer architecture and OOP

--- Dominio/DTO/TipoUsuario.py
+++
@@ -1,9 +0,0 @@
-#Creamos la Clase Tipo Usuario
-class TipoUsuario:
-    #Creamos el constructor
-    def __init__(self,descripcion):
-        self.descripcion=descripcion
-    
-    def __str__(self):
-        return "Descripción: {}".format(self.descripcion)
-    
